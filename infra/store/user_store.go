12200e6622811062945612ef418a7a7e0b0883bd
AnaTofuZ <anatofuz@gmail.com>
2019-12-08 16:20:54 +0900

    user保存系APIのinterfaceを定義
    まずは保存と検索をしようというのがあるのでInterfaceを定義した

---
+++ infra/store/user_store.go
@@ -0,0 +1,15 @@
+package store
+
+import (
+	"context"
+
+	"github.com/anatofuz/BookmarkTarou/model"
+)
+
+//UserStore Userに関するDBアクセスのInterface
+type UserStore interface {
+	GetUsers(ctx context.Context) (*model.Users, error)
+	GetUserFromID(ctx context.Context, uid uint64) (*model.User, error)
+	CreateUser(ctx context.Context, name string, password string) (*model.User, error)
+	UpdateUser(ctx context.Context, uid uint64, name string, password string) (*model.User, error)
+}
--- model/user.go
+++ model/user.go
@@ -5,3 +5,6 @@ type User struct {
 	ID   uint64 `db:"id"`
 	Name string `db:"name"`
 }
+
+//Users Userの集合(テーブル名と対応)
+type Users []User
