d894fc0d9bb86d5fe8d2bb9536dd954fb1f9ba5d
AnaTofuZ <anatofuz@gmail.com>
2019-12-09 12:34:55 +0900

    configパッケージを追加
    diコンテナ的な役割(シュッと他のStore系の初期化を担当するくん)を実装した
    まずはUserだけの機能

---
+++ app/config/config.go
@@ -0,0 +1,63 @@
+package config
+
+import (
+	"fmt"
+	"github.com/anatofuz/BookmarkTarou/infra/store/mysql"
+	"github.com/jmoiron/sqlx"
+	"os"
+	"strconv"
+
+	"golang.org/x/xerrors"
+	"github.com/anatofuz/BookmarkTarou/infra/store"
+)
+
+// AppComponent is a DI container of Bookmark app.
+type AppComponent interface {
+	UserStore() store.UserStore
+}
+
+type appComponentImpl struct {
+	bookmarkDB *sqlx.DB
+}
+
+type Config struct {
+	port int
+	dsn  string
+}
+
+func CreateDiConfig() (*Config, error) {
+	dsn, ok := os.LookupEnv("DATABASE_DSN")
+	if !ok {
+		return nil, fmt.Errorf("failed set DATABASE_DSN")
+	}
+
+	portStr, ok := os.LookupEnv("PORT")
+
+	if !ok {
+		return nil, fmt.Errorf("failed set PORT")
+	}
+
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		return nil, xerrors.Errorf("failed convert integer at port : %w",err)
+	}	
+
+	return &Config{
+		port: port,
+		dsn: dsn,
+	}, nil
+}
+
+// CreateAppComponent DIcontainerのコンストラクタ
+func CreateAppComponent(config *Config) (AppComponent, error) {
+	db, err := sqlx.Open("mysql", config.dsn)
+	if err != nil {
+		return nil, xerrors.Errorf("failed open sqlx : %w",err)
+	}
+
+	return &appComponentImpl{bookmarkDB:db}, nil
+}
+
+func (app *appComponentImpl) UserStore() store.UserStore {
+	return mysql.NewUserStore(app.bookmarkDB)
+}
--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.13
 require (
 	github.com/izumin5210/gex v0.6.0
 	github.com/jmoiron/sqlx v1.2.0
-	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.1.11
 	golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413 // indirect
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
+	google.golang.org/appengine v1.6.5 // indirect
 )
