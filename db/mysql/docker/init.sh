b0a77713e4998fea0f1fc3ec5b4dca59a74dcf68
AnaTofuZ <anatofuz@gmail.com>
2019-12-20 16:03:38 +0900

    mysqlの起動スクリプトのパスを修正

--- db/mysql/docker/init.sh
+++ db/mysql/docker/init.sh
@@ -4,5 +4,5 @@ set -xe
 mysqladmin -uroot create bookmark_tarou
 mysqladmin -uroot create bookmark_tarou_test
 
-mysql -uroot bookmark_tarou < /app/db/schema.sql
-mysql -uroot bookmark_tarou_test < /app/db/schema.sql
+mysql -uroot bookmark_tarou < /app/db/mysql/schema.sql
+mysql -uroot bookmark_tarou_test < /app/db/mysql/schema.sql
