fbfbdcead3f9922c8b0700d42965ebb62769631f
AnaTofuZ <anatofuz@gmail.com>
2019-12-21 10:23:21 +0900

    web層の叩きを実装

--- web/user.go
+++ web/user.go
@@ -2,6 +2,8 @@ package web
 
 import (
 	"fmt"
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/anatofuz/BookmarkTarou/app"
@@ -10,34 +12,42 @@ import (
 )
 
 type userHandler struct {
-	App app.UserApp
+	App       app.UserApp
+	templates map[string]*template.Template
+}
+
+func (u *userHandler) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return u.templates[name].ExecuteTemplate(w, "main.tmpl", data)
 }
 
 func newUserHandler(e *echo.Echo, usrStore store.UserStore) {
 	uh := &userHandler{
-		App: app.NewUserApp(usrStore),
+		App:       app.NewUserApp(usrStore),
+		templates: make(map[string]*template.Template),
 	}
+	uh.templates["index.tmpl"] = template.Must(template.ParseFiles("templates/index.tmpl", "templates/main.tmpl"))
+	e.Renderer = uh
 	e.GET("/", uh.hello)
 	e.GET("/signup", uh.hello)
 }
 
 func (u *userHandler) hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello World!")
+	return c.Render(http.StatusOK, "index.tmpl", nil)
 }
 
 func (u *userHandler) signup(c echo.Context) error {
 	params := new(struct {
-		Name     string `form:name`
-		Password string `form:password`
+		Name     string `form:"name"`
+		Password string `form:"password"`
 	})
 
 	err := c.Bind(params)
 	if err != nil {
 		return fmt.Errorf("failed signup: %w", err)
 	}
-	usr, err := u.App.CreateUser(c.Request().Context(), params.Name, params.Password)
+	usr, err := u.App.Create(c.Request().Context(), params.Name, params.Password)
 	if err != nil {
 		return fmt.Errorf("failed signup: %w", err)
 	}
-	return c.String(http.StatusOK, usr)
+	return c.Render(http.StatusOK, "index.tmpl", usr)
 }
